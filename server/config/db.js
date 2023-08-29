e6051e6105d9ab79ea8248172d7175b6cc4d4ee9
Thi Nguyen <nguyenquethi186@gmail.com>
2023-08-29 16:48:55 -0400

    initial commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
     //local MongoDB deployment ->
-    //"URI": "mongodb://localhost/mydb2"
-  "URI":"mongodb+srv://Arjen:6D9lukYFyINH868e@mongodbserver.vuncwmt.mongodb.net/Survey?retryWrites=true&w=majority",
+    "URI": "mongodb://localhost:27017/mydb2",
+  // "URI":"mongodb+srv://Arjen:6D9lukYFyINH868e@mongodbserver.vuncwmt.mongodb.net/Survey?retryWrites=true&w=majority",
   "Secret": 'SomeSecret'
   };
   
